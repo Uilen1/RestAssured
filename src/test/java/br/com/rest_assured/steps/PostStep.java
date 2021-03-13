9f8ca95d2abd278985a778487ef39d094da41831
Uilen Helei <uilenlelles@hotmail.com>
2021-03-12 23:19:45 -0300

    Casos de testes para mensagem obrigatórias e para inserção de uma movimentação

--- src/test/java/br/com/rest_assured/steps/PostStep.java
+++ src/test/java/br/com/rest_assured/steps/PostStep.java
@@ -14,6 +14,8 @@ import java.util.Map;
 
 import org.junit.Assert;
 
+import br.com.rest_assured.core.MovimentacaoBean;
+
 public class PostStep {
 	
 	private ValidatableResponse Vresponse;
@@ -35,6 +37,44 @@ public class PostStep {
 		;
 	}
 	
+	@Quando("^insiro uma movimentação pelo id \"(.*?)\", descrição \"(.*?)\", envolvido \"(.*?)\",tipo \"(.*?)\", data transação \"(.*?)\", data pagamento \"(.*?)\", valor \"(.*?)\" e status \"(.*?)\" na rota \"(.*?)\"$")
+	public void insiroUmaMovimentaçãoPeloIdDescriçãoEnvolvidoTipoDataTransaçãoDataPagamentoValorEStatusNaRota(String conta_id, String descricao, String envolvido, String tipo, String data_transacao, String data_pagamento, String valor, String status, String rota) throws Throwable {
+	    
+		MovimentacaoBean mov = new MovimentacaoBean();
+		mov.setConta_id(Integer.parseInt(conta_id));
+		mov.setDescricao(descricao);
+		mov.setEnvolvido(envolvido);
+		mov.setTipo(tipo);
+		mov.setData_transacao(data_transacao);
+		mov.setData_pagamento(data_pagamento);
+		mov.setValor(Float.parseFloat(valor));
+		mov.setStatus(Boolean.parseBoolean(status));
+		
+		Vresponse = 
+				given()
+					.header("Authorization", "JWT " + BaseStep.token)
+					.body(mov)
+				.when()
+					.post(rota)
+				.then()
+				;
+		
+	}
+	
+	@Quando("^não insiro nenhum campo obrigatório na rota \"(.*?)\"$")
+	public void nãoInsiroNenhumCampoObrigatórioNaRota(String rota) throws Throwable {
+
+		Vresponse = 
+				given()
+					.header("Authorization", "JWT " + BaseStep.token)
+					.body("{}")
+				.when()
+					.post(rota)
+				.then()
+				;
+		
+	}
+	
 	@Então("^é retornado para a inserção de dados o status code \"(.*?)\"$")
 	public void é_retornado_para_a_inserção_de_dados_o_status_code(String statusCode) throws Throwable {
 
@@ -50,6 +90,25 @@ public class PostStep {
 			.statusCode(is(Integer.parseInt(statusCode)))
 			.body("error", is(mensagem))
 		;
+	}
+	
+	@Então("^valido todas as mensagens obrigatórias e o status code \"(.*?)\"$")
+	public void validoTodasAsMensagensObrigatóriasEOStatusCode(String statusCode) throws Throwable {
+
+		Vresponse
+		.statusCode(is(Integer.parseInt(statusCode)))
+		.body("msg", hasItems(
+					"Data da Movimentação é obrigatório",
+					"Data do pagamento é obrigatório",
+					"Descrição é obrigatório",
+					"Interessado é obrigatório",
+					"Valor é obrigatório",
+					"Valor deve ser um número",
+					"Conta é obrigatório",
+					"Situação é obrigatório"				
+				))
+		;
+		
 	}
 
 }
--- src/test/resources/br/com/rest_assured/features/post/Post.feature
+++ src/test/resources/br/com/rest_assured/features/post/Post.feature
@@ -13,10 +13,28 @@
    Quando insiro uma conta com nome "conta Exemplo2" na rota "/contas"
    Então é retornado para a inserção de dados o status code "201"
    
-   @POSTteste2 
+  @POSTteste2 @Validado
   Cenário: Não deve incluir conta com nome repetido
    Dado que acesso a rota "/signin"
    E logo com email "uilen@hotmail.com" e senha "123456"
    E extraio o token
    Quando insiro uma conta com nome "conta Exemplo2" na rota "/contas"
    Então é retornado para a inserção de dados já existentes o status code "400" com a mensagem de erro "Já existe uma conta com esse nome!"
+
+  @POSTteste3 @Validado
+  Cenário: Deve iserir movimentação com sucesso
+   Dado que acesso a rota "/signin"
+   E logo com email "uilen@hotmail.com" e senha "123456"
+   E extraio o token
+   Quando insiro uma movimentação pelo id "469575", descrição "descriçãoMov", envolvido "envolvidoMov",tipo "REC", data transação "01/12/2020", data pagamento "01/01/2021", valor "100" e status "true" na rota "/transacoes"
+   Então é retornado para a inserção de dados o status code "201"
+   
+  @POSTteste4
+  Cenário: Deve validar campos obrigatórios
+   Dado que acesso a rota "/signin"
+   E logo com email "uilen@hotmail.com" e senha "123456"
+   E extraio o token
+   Quando não insiro nenhum campo obrigatório na rota "/transacoes"
+   Então valido todas as mensagens obrigatórias e o status code "400"   
+   
+   
